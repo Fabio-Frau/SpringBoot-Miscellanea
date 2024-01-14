22f42cf1fc97ca34d328ea23e408f91f2267cfac
Fabio-Frau <fbfr91@yahoo.it>
2024-01-14 11:26:24 +0100

    adding db ex

---
+++ Connect-to-PostgreSQL/src/test/java/com/fabiofrau/Connect/to/PostgreSQL/ConnectToPostgreSqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiofrau.Connect.to.PostgreSQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectToPostgreSqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
